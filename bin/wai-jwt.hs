65a935e9b329408c8449bbc06268f9b4b71a9fbc
uuhan <xuminhui189@gmail.com>
2016-04-16 01:48:51 +0800

    jwt

--- bin/wai-jwt.hs
+++ bin/wai-jwt.hs
@@ -1,8 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
 import           Network.Test.Yesod (yesod_site)
-import           Network.Wai.Middleware.JWT (rewrite)
+import           Network.Wai.Middleware.JWT (jwt)
 import           Network.Wai.Handler.Warp (run)
 
 main :: IO ()
-main = yesod_site >>= run 3000 . rewrite
+main = yesod_site >>= run 3000 . jwt "secret" "x-access-token"
--- src/Network/Wai/Middleware/JWT.hs
+++ src/Network/Wai/Middleware/JWT.hs
@@ -1,45 +1,42 @@
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE PostfixOperators #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Network.Wai.Middleware.JWT
-    ( rewrite
+    ( jwt
     ) 
   where
 
 import           Network.Wai
 import           Web.JWT
+import           Data.Time.Clock.POSIX (getPOSIXTime)
 import qualified Network.HTTP.Types as H
+import           Data.Text (Text)
+import           Data.ByteString (ByteString)
+import           Data.Text.Encoding (decodeUtf8)
+import           Data.CaseInsensitive (mk)
 
-rewrite :: Middleware
-rewrite app req sendRsp = do
-    case (requestMethod req) of
-        m | m == H.methodPost -> do
-            if "x-access-token" `elem` (map fst $ requestHeaders req)
-                then app req sendRsp
-                else app req $ sendRsp . mapResponseStatus (const H.status401)
-          | m == H.methodGet -> do
-            app req $ sendRsp 
-                . mapResponseHeaders f 
-                . mapResponseHeaders (("Location", "https://google.com") :) 
-                . mapResponseStatus (const H.status302)
-        _ -> do
-            sendRsp $ responseLBS H.status401 [] mempty
-  where 
-    f headers = 
-        maybe 
-            ((jwtKey, "_token"):headers)
-            (const headers)
-            (lookup jwtKey headers)
+jwt :: Text -> ByteString -> Middleware
+jwt sec jwtKey app req sendRsp = 
+    let jwtAuth v = do
+            maybe
+                (sendRsp reject)
+                (ifExpired . claims)
+                (decodeAndVerifySignature (secret sec) v)
 
-jwtKey = "x-access-token"
-
-jwt :: Middleware
-jwt app req sendRsp = 
+        ifExpired :: JWTClaimsSet -> IO ResponseReceived
+        ifExpired JWTClaimsSet{..} = do
+            curr <- round <$> getPOSIXTime
+            if maybe (0) (fromEnum . secondsSinceEpoch) exp <=  curr
+                then sendRsp reject
+                else app req sendRsp
+    in
     maybe 
-        (app req $ sendRsp 
-            . mapResponseStatus (const $ H.status401)
-        )
-        (const $ app req sendRsp)
-        (lookup jwtKey headers)
+        (sendRsp reject)
+        (jwtAuth . decodeUtf8)
+        (lookup (mk jwtKey) headers)
   where
     headers = requestHeaders req
+
+reject :: Response
+reject = responseLBS H.status401 [] mempty
